d74e6d31df98a50320988fdde3700ffd4a5f08e5
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2015-07-23 15:01:27 -0500

    fixed bug

--- assembly/procedure_call.s
+++ assembly/procedure_call.s
@@ -10,21 +10,21 @@
 	.global  _start
     
 _start:
-	B	_hello			@ branch to hello procedure
-	B   _exit           @ branch to exit procedure
+	BL	_hello			@ branch to hello procedure with return
+	B   _exit           @ branch to exit procedure with no return
     
 _exit:   
 	MOV R7, #4          @ write syscall, 4
  	MOV R0, #1          @ output stream to monitor, 1
-	MOV R2, #27         @ print string length
-	LDR R1,=hello_str   @ string at label hello_str:
+	MOV R2, #13         @ print string length
+	LDR R1,=exit_str   @ string at label exit_str:
 	MOV R7, #1          @ terminate syscall, 1
 	SWI 0               @ execute syscall
 	
 _hello:
 	MOV R7, #4          @ write syscall, 4
  	MOV R0, #1          @ output stream to monitor, 1
-	MOV R2, #13         @ print string length
+	MOV R2, #27         @ print string length
 	LDR R1,=hello_str   @ string at label hello_str:
 	SWI 0               @ execute syscall
 	MOV PC, LR			@ return
