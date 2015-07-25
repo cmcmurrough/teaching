2be0de99ed7c79910a2ff4a1bf1d7eae73e0a05b
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2015-07-25 01:41:03 -0500

    fixed exit bug in printf.s

--- assembly/printf.s
+++ assembly/printf.s
@@ -11,7 +11,6 @@
     .func main
    
 main:
-    PUSH {LR}           @ push return address to stack
     BL  _print          @ branch to print procedure with return
     B   _exit           @ branch to exit procedure with no return
    
@@ -21,20 +20,20 @@ _exit:
     MOV R2, #21         @ print string length
     LDR R1,=exit_str    @ string at label exit_str:
     SWI 0               @ execute syscall
-    @MOV R7, #1         @ terminate syscall, 1
-    @SWI 0              @ execute syscall
-    POP {PC}            @ pop return address from stack back to PC
+    MOV R7, #1          @ terminate syscall, 1
+    SWI 0               @ execute syscall
        
 _print:
+    MOV R4, LR          @ store LR since printf call overwrites
     LDR R0,=print_str   @ R0 contains formatted string address
     MOV R1, #100        @ printf argument 1
     MOV R2, #200        @ printf argument 2
     MOV R3, #300        @ printf argument 3
     BL printf           @ call printf
-    MOV PC, LR          @ return
+    MOV PC, R4          @ return
  
 .data
 print_str:
-.ascii "Printing 3 numbers: %d %d %d \n"
+.asciz "Printing 3 numbers: %d %d %d \n"
 exit_str:
 .ascii "Terminating program.\n"
