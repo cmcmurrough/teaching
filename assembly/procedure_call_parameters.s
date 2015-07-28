dbbdb73a52cf96341b6d868530da0ae1f74cf5f2
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2015-07-28 15:23:59 -0500

    formatting

--- assembly/procedure_call_parameters.s
+++ assembly/procedure_call_parameters.s
@@ -11,13 +11,13 @@
 	.global  _start
     
 _start:
-    MOV R1, #100        @ define function argument 1
-    MOV R2, #20         @ define function argument 1
-    MOV R3, #3          @ define function argument 1
-	BL  _add            @ branch to hello procedure with return
-    MOV R1, R0          @ copy return value to R1
-    BL _print_val       @ print value stored in R1
-	B   _exit           @ branch to exit procedure with no return
+        MOV R1, #100        @ define function argument 1
+        MOV R2, #20         @ define function argument 1
+        MOV R3, #3          @ define function argument 1
+        BL  _add            @ branch to hello procedure with return
+        MOV R1, R0          @ copy return value to R1
+        BL _print_val       @ print value stored in R1
+        B   _exit           @ branch to exit procedure with no return
     
 _exit:   
 	MOV R7, #4          @ write syscall, 4
@@ -36,9 +36,9 @@ _add:
 	LDR R1,=add_str   @ string at label hello_str:
 	SWI 0               @ execute syscall
     
-    MOV R0, R1          @ copy input register R1 to return register R0
-    ADD R0, R2          @ add input register R2 to return register R0
-    ADD R0, R3          @ add input register R3 to return register R0
+        MOV R0, R1          @ copy input register R1 to return register R0
+        ADD R0, R2          @ add input register R2 to return register R0
+        ADD R0, R3          @ add input register R3 to return register R0
     
 	MOV PC, LR          @ return
     
