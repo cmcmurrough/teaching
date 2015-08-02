8096cf4eca0b72d7891e8116385b326a601acd71
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2015-08-01 22:48:40 -0500

    fixed mod example

--- assembly/mod.s
+++ assembly/mod.s
@@ -13,7 +13,12 @@
 main:
     MOV R1, #200        @ set a constant value for mod evaluation
     MOV R2, #75         @ set a constant value for mod evaluation
-    MOD R3, R1, R2      @ compute the remainder of R1 / R2
+    PUSH {R1}           @ store value to stack
+    PUSH {R2}           @ store value to stack
+    BL  _mod_unsigned   @ compute the remainder of R1 / R2
+    POP {R2}            @ restore values from stack
+    POP {R1}            @ restore values from stack
+    MOV R3, R0          @ copy mod result to R3
     BL  _print          @ branch to print procedure with return
     B   _exit           @ branch to exit procedure with no return
    
@@ -26,17 +31,30 @@ _exit:
     MOV R7, #1          @ terminate syscall, 1
     SWI 0               @ execute syscall
        
+_mod_unsigned:
+    cmp R2, R1          @ check to see if R1 >= R2
+    MOVHS R0, R1        @ swap R1 and R2 if R2 > R1
+    MOVHS R1, R2        @ swap R1 and R2 if R2 > R1
+    MOVHS R2, R0        @ swap R1 and R2 if R2 > R1
+    MOV R0, #0          @ initialize return value
+    B _modloopcheck     @ check to see if
+    _modloop:
+        ADD R0, R0, #1  @ increment R0
+        SUB R1, R1, R2  @ subtract R2 from R1
+    _modloopcheck:
+        CMP R1, R2      @ check for loop termination
+        BHS _modloop    @ continue loop if R1 >= R2
+    MOV R0, R1          @ move remainder to R0
+    MOV PC, LR          @ return
+ 
 _print:
     MOV R4, LR          @ store LR since printf call overwrites
     LDR R0,=print_str   @ R0 contains formatted string address
-    MOV R1, #100        @ printf argument 1
-    MOV R2, #200        @ printf argument 2
-    MOV R3, #300        @ printf argument 3
     BL printf           @ call printf
     MOV PC, R4          @ return
  
 .data
 print_str:
-.asciz "%d % %d = %d \n"
+.asciz "%d mod %d = %d \n"
 exit_str:
 .ascii "Terminating program.\n"
