d41749e11b67c8d330ed6f2d1d0ef94868901b71
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2015-08-04 14:56:28 -0500

    fixed formatting

--- assembly/reg_dump.s
+++ assembly/reg_dump.s
@@ -38,109 +38,109 @@ _exit:
        
 _reg_dump:
     PUSH {LR}           @ backup registers
-	PUSH {R0}           @ backup registers
-	PUSH {R1}           @ backup registers
-	PUSH {R2}           @ backup registers
-	PUSH {R3}           @ backup registers
-	
-	PUSH {R14}          @ push registers for printing
-	PUSH {R13}          @ push registers for printing
-	PUSH {R12}          @ push registers for printing
-	PUSH {R11}          @ push registers for printing
-	PUSH {R10}          @ push registers for printing
-	PUSH {R9}           @ push registers for printing
-	PUSH {R8}           @ push registers for printing
-	PUSH {R7}           @ push registers for printing
-	PUSH {R6}           @ push registers for printing
-	PUSH {R5}           @ push registers for printing
-	PUSH {R4}           @ push registers for printing
-	PUSH {R3}           @ push registers for printing
-	PUSH {R2}           @ push registers for printing
-	PUSH {R1}           @ push registers for printing
+    PUSH {R0}           @ backup registers
+    PUSH {R1}           @ backup registers
+    PUSH {R2}           @ backup registers
+    PUSH {R3}           @ backup registers
+    
+    PUSH {R14}          @ push registers for printing
+    PUSH {R13}          @ push registers for printing
+    PUSH {R12}          @ push registers for printing
+    PUSH {R11}          @ push registers for printing
+    PUSH {R10}          @ push registers for printing
+    PUSH {R9}           @ push registers for printing
+    PUSH {R8}           @ push registers for printing
+    PUSH {R7}           @ push registers for printing
+    PUSH {R6}           @ push registers for printing
+    PUSH {R5}           @ push registers for printing
+    PUSH {R4}           @ push registers for printing
+    PUSH {R3}           @ push registers for printing
+    PUSH {R2}           @ push registers for printing
+    PUSH {R1}           @ push registers for printing
     PUSH {R0}           @ push registers for printing
 	
-	LDR R0,=debug_str   @ prepare register print
+    LDR R0,=debug_str   @ prepare register print
 	
-	MOV R1, #0          @ prepare R0 print
-	POP {R2}            @ prepare R0 print
-	MOV R3, R2          @ prepare R0 print
-	BL printf           @ print R0 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R1 print
-	POP {R2}            @ prepare R1 print
-	MOV R3, R2          @ prepare R1 print
-	BL printf           @ print R1 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R2 print
-	POP {R2}            @ prepare R2 print
-	MOV R3, R2          @ prepare R2 print
-	BL printf           @ print R2 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R3 print
-	POP {R2}            @ prepare R3 print
-	MOV R3, R2          @ prepare R3 print
-	BL printf           @ print R3 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R4 print
-	POP {R2}            @ prepare R4 print
-	MOV R3, R2          @ prepare R4 print
-	BL printf           @ print R4 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R5 print
-	POP {R2}            @ prepare R5 print
-	MOV R3, R2          @ prepare R5 print
-	BL printf           @ print R5 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R6 print
-	POP {R2}            @ prepare R6 print
-	MOV R3, R2          @ prepare R6 print
-	BL printf           @ print R6 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R7 print
-	POP {R2}            @ prepare R7 print
-	MOV R3, R2          @ prepare R7 print
-	BL printf           @ print R7 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R8 print
-	POP {R2}            @ prepare R8 print
-	MOV R3, R2          @ prepare R8 print
-	BL printf           @ print R8 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R9 print
-	POP {R2}            @ prepare R9 print
-	MOV R3, R2          @ prepare R9 print
-	BL printf           @ print R9 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R10 print
-	POP {R2}            @ prepare R10 print
-	MOV R3, R2          @ prepare R10 print
-	BL printf           @ print R10 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R11 print
-	POP {R2}            @ prepare R11 print
-	MOV R3, R2          @ prepare R11 print
-	BL printf           @ print R11 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R12 print
-	POP {R2}            @ prepare R12 print
-	MOV R3, R2          @ prepare R12 print
-	BL printf           @ print R12 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R13 print
-	POP {R2}            @ prepare R13 print
-	MOV R3, R2          @ prepare R13 print
-	BL printf           @ print R13 value prior to reg_dump call
-	
-	MOV R1, #0          @ prepare R14 print
-	POP {R2}            @ prepare R14 print
-	MOV R3, R2          @ prepare R14 print
-	BL printf           @ print R14 value prior to reg_dump call
-	
-	POP {R3}            @ restore register
-	POP {R2}            @ restore register
-	POP {R1}            @ restore register
-	POP {R0}            @ restore regsiter
-	POP {PC}            @ return
+    MOV R1, #0          @ prepare R0 print
+    POP {R2}            @ prepare R0 print
+    MOV R3, R2          @ prepare R0 print
+    BL printf           @ print R0 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R1 print
+    POP {R2}            @ prepare R1 print
+    MOV R3, R2          @ prepare R1 print
+    BL printf           @ print R1 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R2 print
+    POP {R2}            @ prepare R2 print
+    MOV R3, R2          @ prepare R2 print
+    BL printf           @ print R2 value prior to reg_dump call
+ 
+    MOV R1, #0          @ prepare R3 print
+    POP {R2}            @ prepare R3 print
+    MOV R3, R2          @ prepare R3 print
+    BL printf           @ print R3 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R4 print
+    POP {R2}            @ prepare R4 print
+    MOV R3, R2          @ prepare R4 print
+    BL printf           @ print R4 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R5 print
+    POP {R2}            @ prepare R5 print
+    MOV R3, R2          @ prepare R5 print
+    BL printf           @ print R5 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R6 print
+    POP {R2}            @ prepare R6 print
+    MOV R3, R2          @ prepare R6 print
+    BL printf           @ print R6 value prior to reg_dump call
+ 
+    MOV R1, #0          @ prepare R7 print
+    POP {R2}            @ prepare R7 print
+    MOV R3, R2          @ prepare R7 print
+    BL printf           @ print R7 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R8 print
+    POP {R2}            @ prepare R8 print
+    MOV R3, R2          @ prepare R8 print
+    BL printf           @ print R8 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R9 print
+    POP {R2}            @ prepare R9 print
+    MOV R3, R2          @ prepare R9 print
+    BL printf           @ print R9 value prior to reg_dump call
+    
+    MOV R1, #0          @ prepare R10 print
+    POP {R2}            @ prepare R10 print
+    MOV R3, R2          @ prepare R10 print
+    BL printf           @ print R10 value prior to reg_dump call
+    
+    MOV R1, #0          @ prepare R11 print
+    POP {R2}            @ prepare R11 print
+    MOV R3, R2          @ prepare R11 print
+    BL printf           @ print R11 value prior to reg_dump call
+    
+    MOV R1, #0          @ prepare R12 print
+    POP {R2}            @ prepare R12 print
+    MOV R3, R2          @ prepare R12 print
+    BL printf           @ print R12 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R13 print
+    POP {R2}            @ prepare R13 print
+    MOV R3, R2          @ prepare R13 print
+    BL printf           @ print R13 value prior to reg_dump call
+
+    MOV R1, #0          @ prepare R14 print
+    POP {R2}            @ prepare R14 print
+    MOV R3, R2          @ prepare R14 print
+    BL printf           @ print R14 value prior to reg_dump call
+    
+    POP {R3}            @ restore register
+    POP {R2}            @ restore register
+    POP {R1}            @ restore register
+    POP {R0}            @ restore regsiter
+    POP {PC}            @ return
  
 .data
 debug_str:
