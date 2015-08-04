6a6c489371da8801c19f2c68d9d3ea96fe018f57
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2015-08-04 15:18:08 -0500

    fixed prints

--- assembly/reg_dump.s
+++ assembly/reg_dump.s
@@ -11,6 +11,7 @@
     .func main
    
 main:
+    MOV R0, #0          @ set a constant value for printing
     MOV R1, #100        @ set a constant value for printing
     MOV R2, #200        @ set a constant value for printing
     MOV R3, #300        @ set a constant value for printing
@@ -63,72 +64,72 @@ _reg_dump:
     MOV R3, R2          @ prepare R0 print
     BL printf           @ print R0 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R1 print
+    MOV R1, #1          @ prepare R1 print
     POP {R2}            @ prepare R1 print
     MOV R3, R2          @ prepare R1 print
     BL printf           @ print R1 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R2 print
+    MOV R1, #2          @ prepare R2 print
     POP {R2}            @ prepare R2 print
     MOV R3, R2          @ prepare R2 print
     BL printf           @ print R2 value prior to reg_dump call
  
-    MOV R1, #0          @ prepare R3 print
+    MOV R1, #3          @ prepare R3 print
     POP {R2}            @ prepare R3 print
     MOV R3, R2          @ prepare R3 print
     BL printf           @ print R3 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R4 print
+    MOV R1, #4          @ prepare R4 print
     POP {R2}            @ prepare R4 print
     MOV R3, R2          @ prepare R4 print
     BL printf           @ print R4 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R5 print
+    MOV R1, #5          @ prepare R5 print
     POP {R2}            @ prepare R5 print
     MOV R3, R2          @ prepare R5 print
     BL printf           @ print R5 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R6 print
+    MOV R1, #6          @ prepare R6 print
     POP {R2}            @ prepare R6 print
     MOV R3, R2          @ prepare R6 print
     BL printf           @ print R6 value prior to reg_dump call
  
-    MOV R1, #0          @ prepare R7 print
+    MOV R1, #7          @ prepare R7 print
     POP {R2}            @ prepare R7 print
     MOV R3, R2          @ prepare R7 print
     BL printf           @ print R7 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R8 print
+    MOV R1, #8          @ prepare R8 print
     POP {R2}            @ prepare R8 print
     MOV R3, R2          @ prepare R8 print
     BL printf           @ print R8 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R9 print
+    MOV R1, #9          @ prepare R9 print
     POP {R2}            @ prepare R9 print
     MOV R3, R2          @ prepare R9 print
     BL printf           @ print R9 value prior to reg_dump call
     
-    MOV R1, #0          @ prepare R10 print
+    MOV R1, #10          @ prepare R10 print
     POP {R2}            @ prepare R10 print
     MOV R3, R2          @ prepare R10 print
     BL printf           @ print R10 value prior to reg_dump call
     
-    MOV R1, #0          @ prepare R11 print
+    MOV R1, #11         @ prepare R11 print
     POP {R2}            @ prepare R11 print
     MOV R3, R2          @ prepare R11 print
     BL printf           @ print R11 value prior to reg_dump call
     
-    MOV R1, #0          @ prepare R12 print
+    MOV R1, #12         @ prepare R12 print
     POP {R2}            @ prepare R12 print
     MOV R3, R2          @ prepare R12 print
     BL printf           @ print R12 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R13 print
+    MOV R1, #13         @ prepare R13 print
     POP {R2}            @ prepare R13 print
     MOV R3, R2          @ prepare R13 print
     BL printf           @ print R13 value prior to reg_dump call
 
-    MOV R1, #0          @ prepare R14 print
+    MOV R1, #14         @ prepare R14 print
     POP {R2}            @ prepare R14 print
     MOV R3, R2          @ prepare R14 print
     BL printf           @ print R14 value prior to reg_dump call
