9a559095f62da94ee089d118764b4f907c712428
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2016-09-19 10:22:39 -0500

    updated nodejs examples

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,20 +2,32 @@
 var http = require('http');
 
 // define the listening port
-const PORT=8080; 
+const PORT=8080;
 
 // define a function to handle requests and send a response
 function handleRequest(request, response)
 {
-    response.end('Responding to request at path: ' + request.url);
-    console.log('Response sent');
+	// handle all of the chunks of incoming data
+	console.log('Receiving message...')
+	var body = [];
+	request.on('data', function(chunk) {
+  	body.push(chunk);
+	}).on('end', function() {
+		// assemble the entire message from all of the received chunks
+  	body = Buffer.concat(body).toString();
+		console.log(body)
+
+		// send message response
+		response.end('Responding to request at path: ' + request.url);
+		console.log('Response sent');
+	});
 }
 
 // define a function to handle server launch
 function serverStart()
 {
 	// callback triggered when server is successfully listening
-    console.log("Server listening on: http://localhost:%s", PORT);
+	console.log("Server listening on: http://localhost:%s", PORT);
 }
 
 // create a server
@@ -23,4 +35,3 @@ var server = http.createServer(handleRequest);
 
 // start the server
 server.listen(PORT, serverStart);
-
